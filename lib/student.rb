291dfb49599e13671f36830b16245d19b5274010
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 17:25:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,7 @@ attr_reader :id
     INSERT INTO students (name, grade) VALUES (?,?)
     SQL
     DB[:conn].execute(sql,self.name,self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 
