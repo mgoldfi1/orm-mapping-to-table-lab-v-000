3ff2ff7c8c29af641d15e8d8c8d6926a2008664e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 17:40:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,10 @@ attr_accessor :name, :grade
 attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name,grade)
+  def initialize(name,grade, id=nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
