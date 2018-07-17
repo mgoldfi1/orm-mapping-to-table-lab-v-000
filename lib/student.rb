54aac831876552536017e3df135295317319f403
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 17:23:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ attr_accessor :name, :grade, :id
     sql = <<-SQL
     INSERT INTO students (name, grade) VALUES (?,?)
     SQL
-    DB[:conn].execute(sql,self.name,self.grade)
+    @id = DB[:conn].execute(sql,self.name,self.grade)
   end
 
 
