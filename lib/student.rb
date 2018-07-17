f26efc5514849713a861f7e49ebf4bdb1f018dad
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-17 17:08:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+  def initialize(name,grade)
+    @name = name 
+    @grade = grade
 end
